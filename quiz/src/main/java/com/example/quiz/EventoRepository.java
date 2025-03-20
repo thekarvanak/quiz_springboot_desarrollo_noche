d2d90fab54ffc617aeffe9177052dfc0d5cf1113
Andrea Giraldo <andreagiraldo2391@gmail.com>
2025-03-19 22:05:25 -0500

    MODIFICACION funcionalidad busqueda

--- quiz/src/main/java/com/example/quiz/EventoRepository.java
+++ quiz/src/main/java/com/example/quiz/EventoRepository.java
@@ -17,58 +17,67 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class EventoRepository {
-    
-   
-    private final Map<String, Evento> baseDeDatos = new HashMap<>();
-    private final Map<String, String> authTokens = new HashMap<>();
-    private final Map<String, String> tracking = new HashMap<>();
-     
-    public Evento save(Evento evento) {
-        baseDeDatos.put(evento.getId(), evento);
-        return evento;
-    }
-    
-    public Persona savePerson(String idEvento, Persona persona) {
-        baseDeDatos.get(idEvento).getInvitados().add(persona);
-        return persona;
-    }
-    
-    public Persona findPersonByEvent(String idEvento, String idPersona) {
-        Evento evento = baseDeDatos.get(idEvento); 
-        List<Persona> invitados = evento.getInvitados();
-        
-        for (int i=0; i<invitados.size(); i++){
-            if (invitados.get(i).getId() == idPersona){
-                return invitados.get(i);
-            }
-        }
-        return null;
-    }
-    
-    public List<Evento> searchByFilters(LocalDate fecha) {
-        return baseDeDatos.values().stream()
-                .filter(u -> fecha == null || u.getFecha().equals(fecha))
-                .collect(Collectors.toList());
-    }
-    
-    public Evento findByAuthToken(String authToken) {
-        String eventId = authTokens.get(authToken);
-        if (eventId != null) {
-            return baseDeDatos.get(eventId);
-        }
-        return null;
-    }
-    
-    public void deletePersonByEvent(String idEvento, String idPersona) {
-        
-        Persona persona = findPersonByEvent(idEvento,idPersona);
-        
-        if(persona != null){
-            baseDeDatos.get(idEvento).getInvitados().remove(persona);
-        }
-    }
-    
-    
-        
-        
+
+
+	private final Map<String, Evento> baseDeDatos = new HashMap<>();
+	private final Map<String, String> authTokens = new HashMap<>();
+	private final Map<String, String> tracking = new HashMap<>();
+
+	public Evento save(Evento evento) {
+		baseDeDatos.put(evento.getId(), evento);
+		authTokens.put("token-" + evento.getId(), evento.getId());
+		return evento;
+	}
+
+	public Persona savePerson(String idEvento, Persona persona) {
+		baseDeDatos.get(idEvento).getInvitados().add(persona);
+		tracking.put("track-"+persona.getId(), persona.getId());
+		return persona;
+	}
+
+	public Persona findPersonByEvent(String idEvento, String idPersona) {
+		Evento evento = baseDeDatos.get(idEvento); 
+		List<Persona> invitados = evento.getInvitados();
+
+		for (int i=0; i<invitados.size(); i++){
+			if (invitados.get(i).getId() == idPersona){
+				return invitados.get(i);
+			}
+		}
+		return null;
+	}
+
+	public List<Evento> searchByFilters(LocalDate fecha) {
+		return baseDeDatos.values().stream()
+				.filter(u -> fecha == null || u.getFecha().equals(fecha))
+				.collect(Collectors.toList());
+	}
+
+	public Evento findByAuthToken(String authToken) {
+		String eventId = authTokens.get("token-"+authToken);
+		if (eventId != null) {
+			return baseDeDatos.get(eventId);
+		}
+		return null;
+	}
+
+	public void deletePersonByEvent(String idEvento, String idPersona) {
+
+		Persona persona = findPersonByEvent(idEvento,idPersona);
+
+		if(persona != null){
+			baseDeDatos.get(idEvento).getInvitados().remove(persona);
+		}
+	}
+
+	public String findPersonByTrackingId (String idEvento, String idPersona) {
+		Evento evento = baseDeDatos.get(idEvento);
+		if (evento != null) {
+			return tracking.get("track-"+ idPersona);
+		}
+		return null;
+	}
+
+
+
 }
